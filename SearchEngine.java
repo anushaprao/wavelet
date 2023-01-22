ca78e04903f6768487f9d6997ac08c301bb14566
anushaprao <122496338+anushaprao@users.noreply.github.com>
2023-01-22 11:44:55 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,25 +1,34 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     String start = "";
+    ArrayList <String> list = new ArrayList <String>();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Anusha's String: %d", start);
-        } //else if (url.getPath().equals("/search")) {
-            //start += 1;
+            return String.format("Anusha's String: %s", start);
+        } else if (url.getPath().equals("/search")) {
+            for (String element : list){
+                if (element.contains(parameters[1])){
+                    System.out.println(element);
+                }
+
+            }
             //return String.format("Number incremented!");
         //} 
         else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    start += (parameters[1]);
-                    return String.format("String increased by %s! It's now %d", parameters[1], start);
+                if (parameters[0].equals("s")) {
+                    start += (parameters[1] + " ");
+                    list.add(parameters[1]);
+                    return String.format("String increased by %s! It's now %s", parameters[1], start);
                 }
             }
             return "404 Not Found!";
@@ -27,7 +36,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
