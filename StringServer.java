b1c89053220191e1f89759087e50a2e2f18646d7
anushaprao <122496338+anushaprao@users.noreply.github.com>
2023-01-30 00:16:21 -0800

    Added StringServer

--- StringServer.java
+++ StringServer.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Iterator;
+//import java.util.Iterator;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
@@ -11,31 +11,23 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Anusha's String: %s", start);
-        } else if (url.getPath().equals("/search")) {
-            String[] parameters = url.getQuery().split("=");
-            for (String element : list){
-                if (element.contains(parameters[1])){
-                    System.out.println(element);
-                }
-
-            }
-            //return String.format("Number incremented!");
+            return String.format("%s\n", start);
         } 
-        else {
+        else{
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    start += (parameters[1] + " ");
-                    list.add(parameters[1]);
-                    return String.format("String increased by %s! It's now %s", parameters[1], start);
+                for (String element : list){
+                    if (element.contains(parameters[1])){
+                        System.out.println(element);
+                    }
                 }
             }
             return "404 Not Found!";
         }
+            //return "404 Not Found!";
     }
-}
+}   
 
 class SearchEngine {
     public static void main(String[] args) throws IOException {
@@ -49,3 +41,6 @@ class SearchEngine {
         Server.start(port, new Handler());
     }
 }
+/*public class StringServer {
+    
+}*/
